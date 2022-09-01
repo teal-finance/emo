db9e37a820985eecf17da8371c52481f1e6efcd9
Oliver <0uep@hessling.fr>
2022-09-01 23:05:58 +0200

    ✅ Increase test coverage

--- emo_test.go
+++ emo_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestBaseEmo(t *testing.T) {
-	zone := NewZone("testLogger", true, false)
+	zone := NewZone("testLogger", true, false, false)
 	evt := zone.Info("info msg")
 	if evt.Error() != "info msg" {
 		t.Error("got:", evt.Error(), " want:", "info msg")
@@ -15,7 +15,7 @@ func TestBaseEmo(t *testing.T) {
 }
 
 func TestEmoTypes(t *testing.T) {
-	zone := NewZone("testLogger", true, false)
+	zone := NewZone("testLogger", true, false, false)
 	evt := zone.Info("info msg")
 	if evt.Emoji != "ℹ️" {
 		t.Error("got:", evt.Emoji, " want:", "ℹ️")
@@ -23,8 +23,8 @@ func TestEmoTypes(t *testing.T) {
 }
 
 func TestEmo_Error(t *testing.T) {
-	zone := NewZone("testLogger", true, false)
-	err := zone.Info("info msg").Stack(0).Err()
+	zone := NewZone("testLogger", false, false, false)
+	err := zone.Error("error msg").Stack(0).Err()
 	str := fmt.Sprint(err)
 	if err.Error() != str {
 		t.Error("fmt.Sprint(err) = ", str)
@@ -33,7 +33,7 @@ func TestEmo_Error(t *testing.T) {
 }
 
 func TestEmo_ComputeFileLine(t *testing.T) {
-	zone := NewZone("testLogger", true, false)
+	zone := NewZone("testLogger", true, true, false)
 	err := zone.Info("info msg").Stack().Err()
 	prefix := "info msg from "
 	if !strings.HasPrefix(err.Error(), prefix) {
@@ -43,7 +43,7 @@ func TestEmo_ComputeFileLine(t *testing.T) {
 }
 
 func TestEmo_S(t *testing.T) {
-	zone := NewZone("testLogger", true, false)
+	zone := NewZone("testLogger", true, false, false)
 	err := zone.S().Info("info msg").Err()
 	prefix := "info msg from "
 	if !strings.HasPrefix(err.Error(), prefix) {
