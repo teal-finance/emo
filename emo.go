cb2a7ce8483e226f8e5e8e77152ebd998394a640
Oliver <0uep@hessling.fr>
2022-09-01 23:05:58 +0200

    ✨ Call stack info can be forced

--- emo.go
+++ emo.go
@@ -10,16 +10,66 @@ import (
 	color "github.com/acmacalister/skittles"
 )
 
+// ObjectInfo : print debug info about something.
+func ObjectInfo(args ...any) {
+	msg := "[" + color.Yellow("object info") + "] "
+	for _, a := range args {
+		log.Print(msg+"Type: %T Value: %#v", a, a)
+	}
+}
+
 // Logger : base emo logger.
 type Logger struct {
 	Name  string
 	Print bool
 	Color bool
-	Hook  func(Error)
+	Skip  SkipEnum
+	Hook  func(Event)
 }
 
-// Error : base emo event.
-type Error struct {
+type SkipEnum int
+
+const (
+	SkipAuto SkipEnum = 0
+	SkipNo   SkipEnum = -1
+	SkipYes  SkipEnum = 1
+)
+
+// NewLogger : create a logger.
+func NewLogger(name string, args ...bool) Logger {
+	return NewLoggerWithHook(name, nil, args...)
+}
+
+// NewLoggerWithHook : create a Logger with a function hook
+func NewLoggerWithHook(name string, hook func(Event), args ...bool) Logger {
+	print, color, skip := optional(args)
+	return Logger{
+		Name:  name,
+		Print: print,
+		Color: color,
+		Skip:  skip,
+		Hook:  hook,
+	}
+}
+
+// S forces the print of the caller function and file:line.
+// The optional parameter allows to select the position within the stack.
+func (l Logger) S(skip ...int) Logger {
+	l.Skip = SkipYes
+	if len(skip) > 0 {
+		l.Skip = SkipEnum(skip[0])
+	}
+	return l
+}
+
+// S forces the log to be printed.
+func (l Logger) P() Logger {
+	l.Print = true
+	return l
+}
+
+// Event : base emo event.
+type Event struct {
 	Emoji   string
 	Log     Logger
 	IsError bool
@@ -30,62 +80,8 @@ type Error struct {
 	cache   string
 }
 
-// NewLogger : create a logger.
-func NewLogger(name string, args ...bool) Logger {
-	return NewLoggerWithHook(name, nil, args...)
-}
-
-// NewLoggerWithHook : create a Logger with a function hook
-func NewLoggerWithHook(name string, hook func(Error), args ...bool) Logger {
-	print, color := optional(args)
-	return Logger{
-		Name:  name,
-		Print: print,
-		Color: color,
-		Hook:  hook,
-	}
-}
-
-// ObjectInfo : print debug info about something.
-func ObjectInfo(args ...any) {
-	msg := "[" + color.Yellow("object info") + "] "
-	for _, a := range args {
-		log.Print(msg+"Type: %T Value: %#v", a, a)
-	}
-}
-
-func processEvent(emoji string, l Logger, isError bool, args []any) Error {
-	e := newError(emoji, l, isError, args)
-
-	if isError || l.Print {
-		log.Print(e.message())
-	}
-
-	if l.Hook != nil {
-		l.Hook(e)
-	}
-
-	return e
-}
-
-func newError(emoji string, l Logger, isError bool, args []any) Error {
-	pc := make([]uintptr, 1)
-	runtime.Callers(4, pc)
-	f := runtime.FuncForPC(pc[0])
-	file, line := f.FileLine(pc[0])
-
-	return Error{
-		Log:     l,
-		Emoji:   emoji,
-		IsError: isError,
-		Args:    args,
-		From:    f.Name(),
-		File:    file,
-		Line:    line,
-	}
-}
-
-func (e *Error) Error() string {
+// Error is the implementation of the error interface.
+func (e *Event) Error() string {
 	if e.cache == "" {
 		text := make([]string, 0, len(e.Args))
 		for _, a := range e.Args {
@@ -93,7 +89,7 @@ func (e *Error) Error() string {
 		}
 		e.cache = strings.Join(text, " ")
 
-		if e.IsError && e.Log.Print {
+		if e.From != "" {
 			e.cache += " from " + e.Log.bold(e.From) +
 				" in " + e.File + ":" +
 				e.Log.white(strconv.Itoa(e.Line))
@@ -102,7 +98,29 @@ func (e *Error) Error() string {
 	return e.cache
 }
 
-func (e Error) message() string {
+// Stack forces the extraction and print of the call stack info:
+// caller function, file and line number.
+// The optional parameter allows to change the position within the call stack.
+func (e Event) Stack(level ...int) Event {
+	skip := int(e.Log.Skip)
+	if len(level) > 0 {
+		skip += level[0]
+	}
+
+	pc := make([]uintptr, 1)
+	runtime.Callers(skip, pc)
+	f := runtime.FuncForPC(pc[0])
+	e.File, e.Line = f.FileLine(pc[0])
+	e.From = f.Name()
+	return e
+}
+
+// Err converts an Event to a standard Go error.
+func (e Event) Err() error {
+	return &e
+}
+
+func (e Event) message() string {
 	msg := ""
 	if e.Log.Name != "" {
 		msg += "[" + e.Log.yellow(e.Log.Name) + "] "
@@ -117,14 +135,20 @@ func (e Error) message() string {
 	return msg
 }
 
-func optional(args []bool) (bool, bool) {
+func optional(args []bool) (bool, bool, SkipEnum) {
 	switch len(args) {
 	case 0:
-		return true, true
+		return true, true, SkipAuto
 	case 1:
-		return args[0], true
+		return args[0], true, SkipAuto
+	case 2:
+		return args[0], args[1], SkipAuto
 	default:
-		return args[0], args[1]
+		fl := SkipNo
+		if args[2] {
+			fl = SkipYes
+		}
+		return args[0], args[1], fl
 	}
 }
 
@@ -154,4 +178,36 @@ func (l Logger) white(s string) string {
 		return color.White(s)
 	}
 	return s
+}
+
+func processEvent(emoji string, l Logger, isError bool, args []any) Event {
+	e := newEvent(emoji, l, isError, args)
+
+	if isError || l.Print {
+		log.Print(e.message())
+	}
+
+	if l.Hook != nil {
+		l.Hook(e)
+	}
+
+	return e
+}
+
+func newEvent(emoji string, l Logger, isError bool, args []any) Event {
+	e := Event{
+		Log:     l,
+		Emoji:   emoji,
+		IsError: isError,
+		Args:    args,
+		From:    "",
+		File:    "",
+		Line:    0,
+	}
+
+	if (l.Skip == SkipAuto && e.IsError && l.Print) || int(l.Skip) > 0 {
+		e = e.Stack(4)
+	}
+
+	return e
 }
--- emo_test.go
+++ emo_test.go
@@ -1,19 +1,53 @@
 package emo
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestBaseEmo(t *testing.T) {
-	log := NewLogger("testLogger")
-	err := log.Info("infomsg")
-	if err.Error() != "infomsg" {
-		t.Fatal(err.Error(), "!=", "infomsg")
+	log := NewLogger("testLogger", true, false)
+	evt := log.Info("info msg")
+	if evt.Error() != "info msg" {
+		t.Error("got:", evt.Error(), " want:", "info msg")
 	}
 }
 
 func TestEmoTypes(t *testing.T) {
-	log := NewLogger("testLogger")
-	err := log.Info("infomsg")
-	if err.Emoji != "ℹ️" {
-		t.Fatal(err.Emoji, "!=", "ℹ️")
+	log := NewLogger("testLogger", true, false)
+	evt := log.Info("info msg")
+	if evt.Emoji != "ℹ️" {
+		t.Error("got:", evt.Emoji, " want:", "ℹ️")
+	}
+}
+
+func TestEmo_Error(t *testing.T) {
+	log := NewLogger("testLogger", true, false)
+	err := log.Info("info msg").Stack(0).Err()
+	str := fmt.Sprint(err)
+	if err.Error() != str {
+		t.Error("fmt.Sprint(err) = ", str)
+		t.Error("err.Error()     = ", err.Error())
+	}
+}
+
+func TestEmo_ComputeFileLine(t *testing.T) {
+	log := NewLogger("testLogger", true, false)
+	err := log.Info("info msg").Stack().Err()
+	prefix := "info msg from "
+	if !strings.HasPrefix(err.Error(), prefix) {
+		t.Error("got----------->", err)
+		t.Error("want prefix -->", prefix)
+	}
+}
+
+func TestEmo_S(t *testing.T) {
+	log := NewLogger("testLogger", true, false)
+	err := log.S().Info("info msg").Err()
+	prefix := "info msg from "
+	if !strings.HasPrefix(err.Error(), prefix) {
+		t.Error("got----------->", err)
+		t.Error("want prefix -->", prefix)
 	}
 }
